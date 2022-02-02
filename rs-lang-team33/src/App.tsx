502571625e0f99792ba002e9c99370b5728c0859
AliaxeiSehiyenia <alexeisergienia@gmail.com>
2022-02-02 22:06:48 +0300

    feat: install npm  material and icons-material

--- rs-lang-team33/src/App.tsx
+++ rs-lang-team33/src/App.tsx
@@ -1,8 +1,42 @@
 import './App.css';
+import Grid from '@mui/material/Grid';
+import Item from '@mui/material/ListItem';
+import Box from '@mui/material/Box';
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 
 function App() {
   return (
     <div className="App">
+      <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+        <Grid item xs={12}>
+          <Item>
+            <Box sx={{ flexGrow: 1 }}>
+              <AppBar position="static">
+                <Toolbar>
+                  <IconButton
+                    size="large"
+                    edge="start"
+                    color="inherit"
+                    aria-label="menu"
+                    sx={{ mr: 2 }}
+                  >
+                    <MenuIcon />
+                  </IconButton>
+                  <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                    News
+                  </Typography>
+                  <Button color="inherit">Login</Button>
+                </Toolbar>
+              </AppBar>
+            </Box>
+          </Item>
+        </Grid>
+      </Grid>
     </div>
   );
 }
