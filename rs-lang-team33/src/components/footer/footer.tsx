78f2c6ad5ff4b93f1f88934ecf71fb1b2e52ce38
Julia <julia.gramovich@mail.ru>
2022-02-17 23:08:56 +0300

    feat: update links in footer

--- rs-lang-team33/src/components/footer/footer.tsx
+++ rs-lang-team33/src/components/footer/footer.tsx
@@ -1,6 +1,7 @@
 import {
   BottomNavigation,
   BottomNavigationAction,
+  Link,
   SvgIcon,
 } from "@mui/material";
 import "./footer.css";
@@ -14,20 +15,24 @@ const Footer = () => {
     <Box sx={{ alignSelf: "flex-end", width: "100%" }}>
       <BottomNavigation showLabels style={{ background: lightBlue.A200 }}>
         <Box>
-          <SvgIcon
-            className="school-logo"
-            component={RssIcon}
-            inheritViewBox
-            sx={{ width: 80, height: 40, opacity: 0.7 }}
-          />
+          <Link href="https://github.com/rolling-scopes-school/tasks/blob/master/tasks/stage-2/rs-lang/rslang.md">
+            <SvgIcon
+              className="school-logo"
+              component={RssIcon}
+              inheritViewBox
+              sx={{ width: 80, height: 40, opacity: 0.7 }}
+            />
+          </Link>
         </Box>
 
         <BottomNavigationAction
+          href="https://github.com/AliaxeiSehiyenia"
           label="AliaxeiSehiyenia"
           value="AliaxeiSehiyenia"
           icon={<GitHubIcon sx={{ width: 32, height: 32 }} />}
         />
         <BottomNavigationAction
+          href="https://github.com/HulionGH"
           label="HulionGH"
           value="HulionGH"
           icon={<GitHubIcon sx={{ width: 32, height: 32 }} />}
